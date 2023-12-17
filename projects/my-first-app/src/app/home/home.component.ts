964108b6abdbc77d5bfd1351e2d9e1ac2ee25119
Chaitanya Malireddy <malireddyc@vmware.com>
2023-12-17 18:41:22 +0530

    feat(my-first-app): Create interface HousingLocation.
    `npx ng generate interface housingLocation --project=my-first-app`

--- projects/my-first-app/src/app/home/home.component.ts
+++ projects/my-first-app/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+import { HousingLocation } from '../housing-location';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 
 @Component({
@@ -9,4 +10,17 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  readonly baseUrl = 'https://angular.dev/assets/tutorials/common';
+
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test City',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+}
