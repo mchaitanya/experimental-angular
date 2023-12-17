0a4ac339c2a3b654c0a9b4e2a5be32bd221bfd7c
Chaitanya Malireddy <malireddyc@vmware.com>
2023-12-17 18:34:02 +0530

    feat(my-first-app): Create HousingLocationComponent.

--- projects/my-first-app/src/app/home/home.component.ts
+++ projects/my-first-app/src/app/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HousingLocationComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
