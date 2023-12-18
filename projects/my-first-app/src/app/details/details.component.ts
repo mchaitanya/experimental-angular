bc3e5ecd7beaadb6c24d1949c60f920315cf90f3
Chaitanya Malireddy <malireddyc@vmware.com>
2023-12-18 11:08:04 +0530

    feat(my-first-app): Add "Apply now" form to details page.

--- projects/my-first-app/src/app/details/details.component.ts
+++ projects/my-first-app/src/app/details/details.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 import { HousingLocation } from '../housing-location';
@@ -7,7 +8,7 @@ import { HousingService } from '../housing.service';
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css',
 })
@@ -15,10 +16,23 @@ export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
 
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation =
       this.housingService.getHousingLocationById(housingLocationId);
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '', // Default to empty string with the nullish coalescing operator.
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    );
   }
 }
--- projects/my-first-app/src/app/housing.service.ts
+++ projects/my-first-app/src/app/housing.service.ts
@@ -114,9 +114,16 @@ export class HousingService {
   getAllHousingLocations(): HousingLocation[] {
     return this.housingLocations;
   }
+
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housingLocations.find(
       housingLocation => housingLocation.id === id
+    );
+  }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+      `Homes application received - firstName: ${firstName}, lastName: ${lastName}, email: ${email}`
     );
   }
 }
