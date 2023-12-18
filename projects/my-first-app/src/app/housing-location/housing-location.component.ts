be86a48ee96317310a6f0b4af9050cf5b2cf74e1
Chaitanya Malireddy <malireddyc@vmware.com>
2023-12-18 10:16:10 +0530

    feat(my-first-app): Add routerLink to details page for each listing.

--- projects/my-first-app/src/app/housing-location/housing-location.component.ts
+++ projects/my-first-app/src/app/housing-location/housing-location.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css',
 })
