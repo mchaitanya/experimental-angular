14be6d4db69877eea358e7261f7870a464638611
Chaitanya Malireddy <malireddyc@vmware.com>
2023-12-18 16:44:18 +0530

    refactor(my-first-app): Clean up AppComponent.

--- projects/my-first-app/src/app/app.component.ts
+++ projects/my-first-app/src/app/app.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
-import { HomeComponent } from './home/home.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, HomeComponent],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'my-first-app';
-}
+export class AppComponent {}
