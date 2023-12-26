87a7eb1cfbe557ea3b8aa6bf585279b162b71eaf
Chaitanya Malireddy <malireddyc@vmware.com>
2023-12-26 08:21:21 +0530

    test(my-first-app): Refactor HousingService tests.

--- projects/my-first-app/src/app/housing.service.spec.ts
+++ projects/my-first-app/src/app/housing.service.spec.ts
@@ -1,5 +1,10 @@
-import { HttpClient } from '@angular/common/http';
-import { of } from 'rxjs';
+import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { firstValueFrom } from 'rxjs';
 
 import { HousingLocation } from './housing-location';
 import { HousingService } from './housing.service';
@@ -16,41 +21,55 @@ const LOCATION: HousingLocation = {
 };
 
 describe('HousingService', () => {
-  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+  let httpTesting: HttpTestingController;
   let housingService: HousingService;
 
   beforeEach(() => {
-    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
-    housingService = new HousingService(httpClientSpy);
+    TestBed.configureTestingModule({
+      providers: [
+        HousingService,
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
+    });
+    httpTesting = TestBed.inject(HttpTestingController);
+    housingService = TestBed.inject(HousingService);
   });
 
-  it('#getAllHousingLocations should return housing locations.', (done: DoneFn) => {
-    httpClientSpy.get.and.returnValue(of([LOCATION]));
-
-    housingService.getAllHousingLocations().subscribe({
-      next: housingLocations => {
-        expect(housingLocations).toEqual([LOCATION]);
-        done();
-      },
-      error: done.fail,
-    });
-    expect(httpClientSpy.get).toHaveBeenCalledOnceWith(
-      'http://localhost:3000/locations'
-    );
+  afterEach(() => {
+    // Assert no other requests were made.
+    httpTesting.verify();
   });
 
-  it('#getHousingLocationById should return housing location with given id.', (done: DoneFn) => {
-    httpClientSpy.get.and.returnValue(of(LOCATION));
+  it('#getAllHousingLocations should return housing locations.', async () => {
+    const housingLocation$ = housingService.getAllHousingLocations();
+    // Convert the observable to a promise. This also subscribes to the observable, making the HTTP request.
+    const housingLocations = firstValueFrom(housingLocation$);
 
-    housingService.getHousingLocationById(1).subscribe({
-      next: housingLocation => {
-        expect(housingLocation).toEqual(LOCATION);
-        done();
-      },
-      error: done.fail,
-    });
-    expect(httpClientSpy.get).toHaveBeenCalledOnceWith(
-      'http://localhost:3000/locations/1'
+    // Assert the request was made with the HttpTestingController. The request is pending at this stage.
+    const req = httpTesting.expectOne(
+      'http://localhost:3000/locations',
+      'Request to get housing locations'
     );
+    expect(req.request.method).toBe('GET');
+
+    // Flush the request, causing it to complete.
+    req.flush([LOCATION]);
+    await expectAsync(housingLocations).toBeResolvedTo([LOCATION]);
+  });
+
+  it('#getHousingLocationById should return housing location with given id.', async () => {
+    const housingLocation = firstValueFrom(
+      housingService.getHousingLocationById(1)
+    );
+
+    const req = httpTesting.expectOne(
+      'http://localhost:3000/locations/1',
+      'Request to get housing location with id 1'
+    );
+    expect(req.request.method).toBe('GET');
+
+    req.flush(LOCATION);
+    await expectAsync(housingLocation).toBeResolvedTo(LOCATION);
   });
 });
