eaca505d2d28f6efe0724571e6b928a49b0332a5
Chaitanya Malireddy <malireddyc@vmware.com>
2024-01-01 14:17:07 +0530

    test(my-first-app): Fix AppComponent test.

--- projects/my-first-app/src/app/app.component.spec.ts
+++ projects/my-first-app/src/app/app.component.spec.ts
@@ -1,16 +1,23 @@
 import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+
 import { AppComponent } from './app.component';
+
+// Don't need to import RouterTestingModule/RouterLink/RouterOutletStub here since if we provideRouter.
+// @Component({ standalone: true, selector: 'router-outlet', template: '' })
+// class RouterOutletStubComponent {}
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [provideRouter([])],
     }).compileComponents();
   });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(app).toBeDefined();
   });
 });
