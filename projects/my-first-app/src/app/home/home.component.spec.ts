4c65d47382d848dcd76f3d45e385fa781321365d
Chaitanya Malireddy <malireddyc@vmware.com>
2024-01-06 17:35:08 +0530

    test(my-first-app): Add tests for HomeComponent with ng-mocks.

--- projects/my-first-app/src/app/home/home.component.spec.ts
+++ projects/my-first-app/src/app/home/home.component.spec.ts
@@ -5,6 +5,7 @@ import {
   fakeAsync,
   tick,
 } from '@angular/core/testing';
+import { MockBuilder, MockRender, ngMocks } from 'ng-mocks';
 import { defer } from 'rxjs';
 
 import { HousingLocation } from '../housing-location';
@@ -163,6 +164,49 @@ describe('HomeComponent', () => {
         .withContext('Two locations after reset')
         .toHaveSize(2);
     });
+  });
+
+  describe('With ngMocks', () => {
+    beforeEach(() =>
+      MockBuilder(HomeComponent).mock(HousingService, {
+        getAllHousingLocations() {
+          return defer(() => Promise.resolve([LOCATION_HYD, LOCATION_BLR]));
+        },
+      })
+    );
+
+    it('should create', () => {
+      const fixture = MockRender(HomeComponent);
+      expect(fixture.point.componentInstance).toBeDefined();
+    });
+
+    it('should render 2 locations OnInit', fakeAsync(() => {
+      const fixture = MockRender(HomeComponent);
+      tick(); // Allow a tick for getAllHousingLocations to emit.
+      fixture.detectChanges();
+      // Note: ngMocks.findInstances(HousingLocationComponents) fails for some reason.
+      const locationEls = ngMocks.findAll(HousingLocationComponent);
+      expect(locationEls).withContext('Location count').toHaveSize(2);
+      expect(locationEls[0].componentInstance.housingLocation)
+        .withContext('First location is Hyderabad')
+        .toEqual(LOCATION_HYD);
+      expect(locationEls[1].componentInstance.housingLocation)
+        .withContext('Second location is Bangalore')
+        .toEqual(LOCATION_BLR);
+    }));
+
+    it('should filter housing locations', fakeAsync(() => {
+      const fixture = MockRender(HomeComponent);
+      tick();
+      ngMocks.find('input').nativeElement.value = 'hyd';
+      ngMocks.click('button');
+      fixture.detectChanges();
+      const locationEls = ngMocks.findAll(HousingLocationComponent);
+      expect(locationEls).withContext('Location count').toHaveSize(1);
+      expect(locationEls[0].componentInstance.housingLocation)
+        .withContext('Filter matches "Hyderabad"')
+        .toEqual(LOCATION_HYD);
+    }));
   });
 });
 
