154283ae88c9dfc8064ed94ddfa734fe2943f814
Chaitanya Malireddy <malireddyc@vmware.com>
2024-08-31 21:33:05 +0530

    feat(recipe-book): Replace Angular boilerplate with Clarity layout.

--- projects/recipe-book/src/app/app.component.spec.ts
+++ projects/recipe-book/src/app/app.component.spec.ts
@@ -14,18 +14,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'recipe-book' title`, () => {
+  it(`should have the 'Recipe Book' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('recipe-book');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, recipe-book'
-    );
+    expect(app.title).toEqual('Recipe Book');
   });
 });
--- projects/recipe-book/src/app/app.component.ts
+++ projects/recipe-book/src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ClarityIcons, bookIcon } from '@cds/core/icon';
+import { ClrIconModule } from '@clr/angular';
+
+ClarityIcons.addIcons(bookIcon);
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ClrIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'recipe-book';
+  title = 'Recipe Book';
 }
