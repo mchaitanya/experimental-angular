e02cf15fd9e7169ed03cb7dea97210420032b4d7
Chaitanya Malireddy <malireddyc@omnissa.com>
2024-12-01 16:49:16 +0530

    test(recipe-book): Mock RecipeService in AppComponent test.
    Otherwise test fails with 'No provider for HttpClient'.

--- projects/recipe-book/src/app/app.component.spec.ts
+++ projects/recipe-book/src/app/app.component.spec.ts
@@ -1,10 +1,21 @@
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+
 import { AppComponent } from './app.component';
+import { RecipeService } from './core/services';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [
+        {
+          provide: RecipeService,
+          useValue: jasmine.createSpyObj<RecipeService>('RecipeService', {
+            getRecipes: of([]),
+          }),
+        },
+      ],
     }).compileComponents();
   });
 
