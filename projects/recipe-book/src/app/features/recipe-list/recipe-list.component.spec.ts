0250e3a85642d8de5a9132f80921460c78b2c896
Chaitanya Malireddy <malireddyc@omnissa.com>
2024-12-01 16:46:31 +0530

    test(recipe-book): Add test card rendered per recipe.

--- projects/recipe-book/src/app/features/recipe-list/recipe-list.component.spec.ts
+++ projects/recipe-book/src/app/features/recipe-list/recipe-list.component.spec.ts
@@ -1,10 +1,32 @@
-import { DebugElement } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { of } from 'rxjs';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
+import { asyncScheduler, scheduled } from 'rxjs';
 
+import { Recipe } from '@recipe-book/core/models';
 import { RecipeService } from '@recipe-book/core/services';
 import { RecipeListComponent } from './recipe-list.component';
+
+const RECIPE1: Recipe = {
+  id: '1',
+  title: 'Recipe 1',
+  imageUrl: '/resources/recipe1.jpg',
+  servings: 2,
+  ingredients: ['Ingredient 1'],
+  steps: ['Step 1'],
+};
+
+const RECIPE2: Recipe = {
+  id: '2',
+  title: 'Recipe 2',
+  imageUrl: '/resources/recipe2.jpg',
+  servings: 2,
+  ingredients: ['Ingredient 1'],
+  steps: ['Step 1'],
+};
 
 describe('RecipeListComponent', () => {
   let fixture: ComponentFixture<RecipeListComponent>;
@@ -17,7 +39,7 @@ describe('RecipeListComponent', () => {
         {
           provide: RecipeService,
           useValue: jasmine.createSpyObj<RecipeService>('RecipeService', {
-            getRecipes: of([]),
+            getRecipes: scheduled([[RECIPE1, RECIPE2]], asyncScheduler),
           }),
         },
       ],
@@ -26,13 +48,26 @@ describe('RecipeListComponent', () => {
     component = fixture.componentInstance;
   });
 
+  beforeEach(fakeAsync(() => {
+    fixture.detectChanges(); // Call ngOnInit()
+    tick(); // Flush the observable from getRecipes
+  }));
+
   it('should create', () => {
     expect(component).toBeDefined();
   });
 
-  it('should contain div.clr-row', () => {
-    const debugEl: DebugElement = fixture.debugElement;
-    const divDe: DebugElement = debugEl.query(By.css('div.clr-row'));
-    expect(divDe).toBeDefined();
+  it('should render a card per recipe', () => {
+    fixture.detectChanges(); // Update the bindings
+    const cardEls = (<HTMLElement>fixture.nativeElement).querySelectorAll(
+      '.card'
+    );
+    expect(cardEls).withContext('Card count').toHaveSize(2);
+    expect(cardEls[0].textContent)
+      .withContext('First recipe')
+      .toContain('Recipe 1');
+    expect(cardEls[1].textContent)
+      .withContext('Second recipe')
+      .toContain('Recipe 2');
   });
 });
