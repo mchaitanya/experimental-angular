e5f88795a7498796af4015475a3f3ce599e6c7af
Chaitanya Malireddy <malireddyc@omnissa.com>
2024-12-25 17:22:34 +0530

    feat(recipe-book): Add recipeFilter$ in RecipeService.

--- projects/recipe-book/src/app/core/services/recipe.service.ts
+++ projects/recipe-book/src/app/core/services/recipe.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { catchError, Observable, of } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, catchError, Observable, of } from 'rxjs';
 
-import { Recipe } from '@recipe-book/core/models';
+import { EMPTY_FILTER, Recipe, RecipeFilter } from '@recipe-book/core/models';
 import { environment } from '@recipe-book/environments/environment';
 
 const BASE_PATH = environment.basePath;
@@ -11,11 +11,21 @@ const BASE_PATH = environment.basePath;
   providedIn: 'root',
 })
 export class RecipeService {
-  private http = inject(HttpClient);
+  public recipeFilter$: Observable<RecipeFilter>;
+  private recipeFilterSubject: BehaviorSubject<RecipeFilter>;
+
+  constructor(private http: HttpClient) {
+    this.recipeFilterSubject = new BehaviorSubject<RecipeFilter>(EMPTY_FILTER);
+    this.recipeFilter$ = this.recipeFilterSubject.asObservable();
+  }
 
   getRecipes(): Observable<Recipe[]> {
     return this.http
       .get<Recipe[]>(`${BASE_PATH}/recipes`)
       .pipe(catchError(() => of([]))); // Fall back to empty array on error
+  }
+
+  updateRecipeFilter(filter: RecipeFilter) {
+    this.recipeFilterSubject.next(filter);
   }
 }
